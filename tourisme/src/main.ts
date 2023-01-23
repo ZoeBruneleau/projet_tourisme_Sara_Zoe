85263cb1c7210c76982416293863b7d8b66948fa
sara <saraelallouche@hotmail.fr>
2023-01-23 12:24:32 +0100

    add index et main

--- tourisme/src/main.ts
+++ tourisme/src/main.ts
@@ -1,12 +1,15 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { Server } from 'miragejs';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+new Server({
+  routes(){
+    this.passthrough();
+    this.get('getTourism', ()=> require("./app/mock.tourisme.json"))
+  }
+})
+
