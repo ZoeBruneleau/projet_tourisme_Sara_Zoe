1a8c48dde1c3be08f78914f2b2ebde054a1ecea6
Zoé Bruneleau <zoebruneleau@macbook-pro-de-zoe.home>
2023-02-13 18:12:52 +0100

    deafult path

--- tourisme/src/app/app-routing.module.ts
+++ tourisme/src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   { path : 'login', component: LoginComponent },
   { path : 'add_comment/:id', component: AddCommentComponent },
   { path : 'top5', component: TopComponent },
-  { path : '', component:HomeComponent },
+  { path : '', redirectTo :'home'},
   { path : '**', component:NotFoundComponent }
   ]
 
